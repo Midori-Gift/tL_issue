86f1bb3f8e0a5e3fd382209560297865b298eb73
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-12 08:56:00 +0000

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
   resources :users, only:[:show, :edit]
   
   # 投稿関連ルーティング
-  resources :posts, only:[:new, :index, :show, :create] do
-    resource :postcomment, only:[:create, :destroy]
-    resource :favorite, only:[:create, :destroy]
+  resources :posts, only:[:new, :index, :show, :create, :destroy] 
+  
+  resources :posts do
+    resources :post_comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
   end
-  # Prefixの紛らわしさから投稿削除は個別で設定
-  delete 'post/:id/destroy' => 'posts#destroy', as:'post_destroy'
+  
 end
