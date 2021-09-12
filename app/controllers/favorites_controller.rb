a8d3ef02b1ff96c7418e91ce0165b1fa4ff02099
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-12 11:10:15 +0000

    いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
   
   def create
+    select_post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: select_post.id)
+    favorite.save
+    redirect_to post_path(select_post)
   end
   
   def destroy
+    select_post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: select_post.id)
+    favorite.destroy
+    redirect_to post_path(select_post)
   end
   
 end
