257ca1f8225ed70b9c5aa8719c6bdb0fbec20567
Midori-Gift <okometaberoyo5009@gmail.com>
2021-09-15 07:07:48 +0000

    デザイン詰め中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  # ログイン後の遷移先を変更 2021/09/15
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+  
   protected
 
   def configure_permitted_parameters
